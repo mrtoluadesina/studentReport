7a2974824224316b0e9b41fbd12fd944242a12b1
Tolu Adesina <tadesina90@gmail.com>
2019-04-22 23:19:14 +0100

    new property in user to help get user type

--- user/users.js
+++ user/users.js
@@ -1,9 +1,10 @@
 var db = require('../db');
 
-function User(name, email, password) {
+function User(name, email, password, userType) {
   this.name = name;
   this.email = email;
   this.password = password;
+  this.userType = userType || 'admin';
   this.id = db.users.length > 0 ? db.users[db.users.length - 1].id + 1 : 1; 
   db.users.push(this);
 }
