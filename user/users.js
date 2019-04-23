9b272fa67bfc835b8acbb0777928453f1422687e
Tolu Adesina <tadesina90@gmail.com>
2019-04-23 05:35:43 +0100

    Method for creating a student record add to User prototype, and condition to make sure a student cannot create a record implemented

--- user/users.js
+++ user/users.js
@@ -1,12 +1,24 @@
 var db = require('../db');
+var studentRecord = require('../record/record');
 
 function User(name, email, password, userType) {
   this.name = name;
   this.email = email;
   this.password = password;
-  this.userType = userType || 'admin';
+  this.userType = userType || 'student';
   this.id = db.users.length > 0 ? db.users[db.users.length - 1].id + 1 : 1; 
   db.users.push(this);
+}
+
+User.prototype.createRecord = function(grade, remark, student_id) {
+  if (this.userType !== 'student') {
+    var addRecord = studentRecord.prototype.newRecord()
+    addRecord.grade = grade;
+    addRecord.remark = remark;
+    addRecord.student_id = student_id;
+    db.studentRecords.push(addRecord);
+    return 'Student Record Successfully added';
+  } return 'You do not have permission to do this';
 }
 
 module.exports = User;
