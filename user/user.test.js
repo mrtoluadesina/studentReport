252ddef39c2d64cb5251fedb40b61b9ec464ad99
Tolu Adesina <tadesina90@gmail.com>
2019-04-23 05:34:47 +0100

    Test Case - Test for when a record is created to know if the user has permission or not and then if the record is created

--- user/user.test.js
+++ user/user.test.js
@@ -1,10 +1,18 @@
 var db = require('../db');
 var User = require('./users');
+var Record = require('../record/record');
 
 test('Checking for instance of User', function() {
   var length = db.users.length;
   expect(length).toBe(0);
-  var jon = new User('Job Snow', 'jonsnow@housestark.got', 'iknownothing');
+  var jon = new User('Jon Snow', 'jonsnow@housestark.got', 'iknownothing');
   expect(jon instanceof User).toBeTruthy();
   expect(length + 1).toBe(1);
-})
+});
+
+test('Creating a student Record', function() {
+  var jon = new User ('Jon Snow', 'jonsnow@housestark.got', 'iknownothing', 'teacher');
+  var arya = new User('Arya Stark', 'aryastark@housestark.got', 'agirlhasnoface');
+  expect(jon.createRecord('4.3', 'Very brilliant chap', '0032')).toBe('Student Record Successfully added');
+  expect(arya.createRecord('3.0', 'Decent Attempt', '0012')).toBe('You do not have permission to do this');
+});
