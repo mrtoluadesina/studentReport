c8e5a7cbc5055897e5109b8932a7f16ad2df8ca5
Tolu Adesina <tadesina90@gmail.com>
2019-04-23 06:21:06 +0100

    Test Case - editing a student record and the correct data is seen in the database after edit

--- user/user.test.js
+++ user/user.test.js
@@ -11,7 +11,7 @@ test('Checking for instance of User', function() {
 });
 
 test('Creating a student Record', function() {
-  var jon = new User ('Jon Snow', 'jonsnow@housestark.got', 'iknownothing', 'teacher');
+  var jon = new User('Jon Snow', 'jonsnow@housestark.got', 'iknownothing', 'teacher');
   var arya = new User('Arya Stark', 'aryastark@housestark.got', 'agirlhasnoface');
   expect(jon.createRecord('4.3', 'Very brilliant chap', '0032')).toBe('Student Record Successfully added');
   expect(arya.createRecord('3.0', 'Decent Attempt', '0012')).toBe('You do not have permission to do this');
@@ -26,3 +26,10 @@ test('Reading a student record by student Id', function() {
   var cersei = new User('Cersei Lannister', 'ceisei@houselannister.got', 'neckkk', 'admin');
   expect(cersei.readByStudentId('0032')).toEqual(expect.arrayContaining([expect.objectContaining({student_id : '0032'})]));
 })
+
+test('Editing a Student Record', function() {
+  var jon = new User('Jon Snow', 'jonsnow@housestark.got', 'iknownothing', 'teacher');
+  var petyr = new User('Petyr Baelish', 'petyr@housebaelish.got', 'iamlittlefinger', 'parent');
+  expect(jon.updateStudentRecord(1, '4.5', 'This young chap keeps breaking his own records')).toMatch('Student Record');
+  expect(petyr.updateStudentRecord(1, '4.34', 'Nothing lasts, so keep working hard')).toBe('You do not have permission to do this');
+});
